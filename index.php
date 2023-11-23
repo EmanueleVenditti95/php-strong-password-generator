6f6e7beb3271d006f9cfb58b4cb272d93771a66b
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-23 16:20:12 +0100

    Add - pw generator with length defined by input

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <?php
-function getRandomPassword () {
+// recupero la lunghezza della password dall'input dell'utente
+$passwordLength = ($_GET['password_length']);
+
+//definisco la funzione di generazione password
+function getRandomPassword ($passwordLength) {
 
 $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!()-.?[]_~;:@#§*£&';
 
 $shuffledChars = str_shuffle($characters);
 
-$passwordString = mb_substr($shuffledChars,0,8);
+$passwordString = mb_substr($shuffledChars,0,$passwordLength);
 
 var_dump($passwordString);
 }
+
+//invoco la funzione
+getRandomPassword($passwordLength);
 ?>
 
-<form action="GET">
-    <input type="text">
+<form action="" method="GET">
+    <input type="text" name="password_length">
     <input type="submit">
 </form>
 
-<?php getRandomPassword(); ?>
